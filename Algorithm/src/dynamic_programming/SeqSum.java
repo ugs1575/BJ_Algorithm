da394b184db81df3027056fd91582e0fb515f9ac
우경서 <@DESKTOP-KR5RRLS>
2019-09-05 17:01:01 +0900

    20190905 연속합

---
+++ Algorithm/src/dynamic_programming/SeqSum.java
@@ -0,0 +1,31 @@
+package dynamic_programming;
+import java.util.*;
+
+public class SeqSum {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] a = new int[n];
+		int[] d = new int[n];
+		
+		for(int i=0; i<n; i++) {
+			a[i] = sc.nextInt();
+			d[i] = a[i];
+		}
+		
+		for(int i=1; i<n; i++) {
+			if(d[i-1]+a[i]>d[i]) {
+				d[i] = d[i-1]+a[i];
+			}
+		}
+		
+		int ans = d[0];
+		for(int i=0; i<n; i++){
+			if(d[i]>ans) {
+				ans = d[i];
+			}
+		}
+		
+		System.out.println(ans);
+	}
+}
