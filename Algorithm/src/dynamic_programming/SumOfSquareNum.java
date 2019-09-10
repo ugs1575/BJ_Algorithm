dc94552da60016706719a10fa1b8a821e7e8ddda
우경서 <@DESKTOP-KR5RRLS>
2019-09-10 09:43:31 +0900

    20190910 제곱수의 합

---
+++ Algorithm/src/dynamic_programming/SumOfSquareNum.java
@@ -0,0 +1,27 @@
+package dynamic_programming;
+import java.util.*;
+
+public class SumOfSquareNum {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] d = new int[n+1];
+		
+		d[1] = 1;
+		for(int i=2; i<=n; i++) {
+			d[i] = i;
+			for(int j=1; j*j<=i; j++) {
+				 if(d[i] > d[i-j*j]+1) {
+					 d[i] = d[i-j*j]+1;
+				 }
+			
+			}
+		}
+		
+		
+		
+		System.out.print(d[n]);
+		
+		
+	}
+}
