33506d23230c7b58f6fcb374602bd0037d446ef5
Gyeongseo Woo <ugs1575@gmail.com>
2019-10-01 08:30:22 +0900

    191001 최초공약수와 최대공배수

---
+++ Algorithm/src/dynamic_programming/GCD.java
@@ -0,0 +1,24 @@
+package dynamic_programming;
+import java.util.*;
+
+public class GCD {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int ans = gcd(a,b);
+		int ans2 = ans*(a/ans)*(b/ans);
+		
+		System.out.println(ans);
+		System.out.println(ans2);
+	}
+	
+	public static int gcd(int a, int b) {
+		int r = a%b;
+		if(r==0) {
+			return b;
+		}else {
+			return gcd(b,r);
+		}
+	}
+}
