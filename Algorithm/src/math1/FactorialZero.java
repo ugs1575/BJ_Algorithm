993d7ff20909632926f7446ab2b1f095340844cc
gswoo <gswoo@DESKTOP-KR5RRLS>
2019-10-15 17:32:09 +0900

    191015 2진수 8진수

---
+++ Algorithm/src/math1/FactorialZero.java
@@ -0,0 +1,38 @@
+package math1;
+import java.util.*;
+
+public class FactorialZero {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		long n = sc.nextLong();
+		long ans = fac(n);
+		System.out.println(ans);
+		/*String ans2 = Integer.toString(ans);
+		char[] char_arr = ans2.toCharArray();
+		int cnt = 0;
+		for(int i=char_arr.length-1; i>=0; i--) {
+			int num = Character.getNumericValue(char_arr[i]);
+			if(num == 0) {
+				cnt ++;
+			}else{
+				break;
+			}
+		}
+		
+		System.out.print(cnt);*/
+		
+	}
+	
+	public static long fac(long n) {
+		Long one = new Long(1);
+		Long zero = new Long(0);
+		Long nObj = new Long(n);
+		int compareOne = nObj.compareTo(one);
+		int compareZero = nObj.compareTo(zero);
+		if( compareOne == 0 || compareZero == 0 ) {
+			return one;
+		}else {
+			return n*fac(n-1);
+		}
+	}
+}
--- Algorithm/src/math1/OctoToDeci.java
+++ Algorithm/src/math1/OctoToDeci.java
@@ -5,18 +5,21 @@ public class OctoToDeci {
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
 		String n = sc.next();
-		String str = "";
-		String ans = "";
+		StringBuilder ans = new StringBuilder();
 		String[] arr = {"000", "001", "010", "011", "100", "101", "110", "111"};
 		
 		for(int i=0; i<n.length(); i++) {
 			int num = Character.getNumericValue(n.charAt(i));
-			str += arr[num];
+			String str = arr[num];
+			ans.append(str);
 		}
 		
-		int compare = Character.compare(str.charAt(0), '0');
-		if(compare == 0) {
-			ans = str.substring(1);
+		int compare = Character.compare(ans.charAt(0), '0');
+		if(compare == 0 && n.length()!=1) {
+			ans.delete(0,1);
+		}else if(ans.equals("0") || ans.equals("1")) {
+			ans.delete(0,2);
+			System.out.print("hi");
 		}
 		
 		System.out.print(ans);
