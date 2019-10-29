f5fea7db06e7171ad12d8150da712d54174644b6
gswoo <gswoo@DESKTOP-KR5RRLS>
2019-10-29 09:26:39 +0900

    191029 골드바흐의 추측

--- Algorithm/src/math1/AssumtionOfGoldBahu.java
+++ Algorithm/src/math1/AssumtionOfGoldBahu.java
@@ -2,51 +2,38 @@ package math1;
 import java.util.*;
 
 public class AssumtionOfGoldBahu {
+	public static final int MAX = 1000000;
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
 		ArrayList<Integer> prime = new ArrayList<>();
-		boolean[] isPrime = new boolean[1000001];
+		boolean[] isPrime = new boolean[MAX+1];
 		
 		isPrime[0] = isPrime[1] = true;
 		
-		for(int i=2; i<=1000000; i++) {
+		for(int i=2; i*i<=MAX; i++) {
 			if(isPrime[i] == false) {
 				prime.add(i);
-				for(int j = i*i; j<=1000000; j+=i) {
+				for(int j = i*i; j<=MAX; j+=i) {
 					isPrime[j] = true;
 				}
+			}else {
+				continue;
 			}
 		}
 		
-		int a = 0;
-		int b = 0;
-		int c = 0;
 		
 		while(true) {
 			int n = sc.nextInt();
 			if(n==0) break;
 			
 			for(int i=0; i<prime.size(); i++) {
-				int tmp = n - prime.get(i);
-				
-				if(tmp%2==0) {
-					continue;
-				}else {
-					boolean p = prime.contains(tmp);
-					if(p) {
-						int a2 = prime.get(i);
-						int b2 = tmp;
-						int c2 = b-a;
-						if(c2>c) {
-							a = a2;
-							b = b2; 
-							c= c2;
-						}
-					}
+				int p = prime.get(i);
+				if(isPrime[n-p] == false) {
+					System.out.println(n+" = "+p+" + "+(n-p));
+					break;
 				}
 			}
 			
-			System.out.println(n+" = "+a+" + "+b);
 		}
 	}
 }
