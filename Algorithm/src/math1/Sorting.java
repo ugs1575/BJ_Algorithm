3c1c6df80e953d5b1fc3c6741856d8113270d21b
gswoo <gswoo@DESKTOP-KR5RRLS>
2019-11-04 17:40:31 +0900

    191104 수 정렬하기2

---
+++ Algorithm/src/math1/Sorting.java
@@ -0,0 +1,32 @@
+package math1;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+/*
+5
+5
+4
+3
+2
+1*/
+
+public class Sorting {
+	public static void main(String args[]) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		for(int i=0; i<n; i++) {
+			int num = Integer.parseInt(br.readLine());
+			arr.add(num);
+		}
+		Collections.sort(arr);
+		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		for(int i=0; i<n; i++) {
+			bw.write(arr.get(i)+"\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+}
