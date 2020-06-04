0a48d90fd66748c4a8cfb29861d5822e17f0aa30
gswoo <ugs1575@naver.com>
2020-06-04 16:03:44 +0900

    200604 n과 m(1), geeks 순열 예제 코드

---
+++ Algorithm/src/bruteForce/NM1_15649.java
@@ -0,0 +1,42 @@
+package bruteForce;
+
+import java.util.Scanner;
+
+public class NM1_15649 {
+	static boolean[] check;
+	static int[] ans;
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		check = new boolean[n+1];
+		ans = new int[m];
+		
+		permute(0, n, m);
+		
+	}
+	
+	public static void permute(int index, int n, int m) {
+		if(index == m) {
+			for(int i=0; i<index; i++) {
+				System.out.print(ans[i]+" ");
+			}
+			
+			System.out.println();
+		}else {
+			for(int i=1; i<=n; i++) {
+				if(check[i]) continue;
+				ans[index] = i;
+				check[i] = true;
+				if(index+1 <= m) {
+					permute(index+1, n, m);
+					check[i] = false;
+				}
+			}
+		}
+		
+		
+	}
+	
+}
