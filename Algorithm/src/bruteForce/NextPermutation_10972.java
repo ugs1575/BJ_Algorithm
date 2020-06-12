baa953706aef417a032542460a93e61825c7cd54
gswoo <ugs1575@naver.com>
2020-06-12 10:26:04 +0900

    200612 다음 순열

--- Algorithm/src/bruteForce/NextPermutation_10972.java
+++ Algorithm/src/bruteForce/NextPermutation_10972.java
@@ -13,7 +13,7 @@ package bruteForce;
 import java.util.*;
 
 public class NextPermutation_10972 {
-	static int n;
+	static int n, cnt, ansCnt;
 	static int[] arr, ans;
 	static boolean[] check;
 	static LinkedHashSet<String> lhs;
@@ -29,32 +29,22 @@ public class NextPermutation_10972 {
 		ans = new int[n];
 		String permutation = "";
 		find = false;
-		
+		ansCnt = 1;
+		cnt = 0;
+				
 		for(int i=0; i<n; i++) {
 			permutation += sc.nextInt();
 			if(i != n-1) permutation += " ";
 			arr[i] = i+1;
+			ansCnt = ansCnt*(i+1);
 		}
 		
-		
 		permute(0, permutation);
-		/*Iterator<String> i = lhs.iterator();
+		
+		Iterator<String> i = lhs.iterator();
 	
-		while(i.hasNext()) {
-			String value = i.next().toString();
-			
-			if(find) {
-				System.out.println(value);
-				break;
-			}
-			
-			if(value.equals(permutation)) {
-				find = true;
-				if(!i.hasNext())
-					System.out.println(-1);
-			}
-			
-		}*/
+		String value = i.next().toString();
+		System.out.println(value);
 		
 		
 	}
@@ -66,20 +56,28 @@ public class NextPermutation_10972 {
 				sb.append(ans[i]);
 				if(i != n-1) sb.append(" ");
 			}
-			if(sb.toString() == permutation) {
-				find = true;
-			}
+			
+			
+			cnt ++;
 			
 			if(find) {
 				lhs.add(sb.toString());
 				find = false;
+			}else if(sb.toString().equals(permutation)) {
+				find = true;
+				if(cnt == ansCnt) {
+					int minusOne = -1;
+					lhs.add(String.valueOf(minusOne));
+				}
 			}
+			
+			
 		}else {
 			for(int i=0; i<n; i++) {
 				if(check[arr[i]]) continue;
 				ans[index] = arr[i];
 				check[arr[i]] = true;
-				permute(index+1);
+				permute(index+1, permutation);
 				check[arr[i]] = false;
 			}
 		}
