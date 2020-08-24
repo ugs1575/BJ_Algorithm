6191732c5c15dc567623e77751453f9bd3abe68b
gswoo <ugs1575@naver.com>
2020-08-25 08:47:08 +0900

    200825 재귀 패키지 생성 및 파일 정리

---
+++ Algorithm/src/recursion/Recursion_14889.java
@@ -0,0 +1,75 @@
+package recursion;
+
+import java.util.Scanner;
+
+public class Recursion_14889 {
+    static int n, max, min;
+    static int[][] arr;
+    static boolean[] check;
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        n     = sc.nextInt();
+        arr   = new int[n][n];
+        check = new boolean[n];
+        max   = n/2;
+        min   = Integer.MAX_VALUE;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        assignTeam(0, 0, true);
+
+        System.out.println(min);
+    }
+
+    public static void assignTeam(int index, int cnt, boolean team){
+        if(index >= n || cnt >= max){
+            return;
+        }
+
+        if(team){
+            check[index] = true;
+        }else{
+            check[index] = false;
+        }
+
+        if(cnt == max-1){
+            int d = getDiff();
+            if(d < min) min = d;
+            return;
+        }
+
+        assignTeam(index+1, cnt+1, true);
+        assignTeam(index+1, cnt, false);
+    }
+
+
+    public static int getDiff(){
+        int startPower = 0;
+        int linkPower  = 0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(i == j) continue;
+                if(check[i] && check[j]){
+                    startPower += arr[i][j];
+                }
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(i == j) continue;
+                if(!check[i] && !check[j]){
+                    linkPower += arr[i][j];
+                }
+            }
+        }
+
+        int ans = Math.abs(startPower - linkPower);
+
+        return ans;
+    }
+}
