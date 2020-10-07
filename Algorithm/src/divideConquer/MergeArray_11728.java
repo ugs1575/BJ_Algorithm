d1b43a023baa55f9fde61139481f9a67f1428540
wgs <wgs@172.30.1.12>
2020-10-07 14:31:38 +0900

    200929 배열합치기, 종이의개

---
+++ Algorithm/src/divideConquer/MergeArray_11728.java
@@ -0,0 +1,70 @@
+package divideConquer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MergeArray_11728 {
+    public static void merge(int start, int mid, int end, int[] a){
+        int i = start;
+        int j = mid+1;
+        int k = 0;
+        int[] b = new int[end-start];
+        while (i<=mid && j<=end){
+            if(a[i] <= a[j]){
+                b[k++] = a[i++];
+            }else{
+                b[k++] = a[j++];
+            }
+        }
+
+        while (i <= mid) b[k++] = a[i++];
+        while (j <= end) b[k++] = a[j++];
+        for(int t=start; t<=end; t++){
+            a[t] = b[t-start];
+        }
+
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        int n = Integer.valueOf(s[0]);
+        int m = Integer.valueOf(s[1]);
+        int[] a = new int[n];
+        int[] b = new int[m];
+        int[] ans = new int[n+m];
+        s = br.readLine().split(" ");
+        for(int i=0; i<n; i++){
+            a[i] = Integer.valueOf(s[i]);
+        }
+        s = br.readLine().split(" ");
+        for(int i=0; i<m; i++){
+            b[i] = Integer.valueOf(s[i]);
+        }
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while (i<n && j<m){
+            if(a[i] <= b[j]){
+                ans[k++] = a[i++];
+            }else{
+                ans[k++] = b[j++];
+            }
+        }
+
+        while (i < n) ans[k++] = a[i++];
+        while (j < m) ans[k++] = b[j++];
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int t=0; t<n+m; t++){
+            sb.append(ans[t]+" ");
+        }
+
+        System.out.println(sb);
+
+    }
+}
---
+++ Algorithm/src/divideConquer/Paper_1780.java
@@ -0,0 +1,55 @@
+package divideConquer;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Paper_1780 {
+    public static boolean same(int[][] arr, int x, int y, int n){
+        for(int i=x; i<x+n; i++){
+            for(int j=y; j<y+n; j++){
+                if(arr[x][y] != arr[i][j]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void solve(int[][] arr, int[] ans, int x, int y, int n){
+        if(same(arr, x, y, n)){
+            ans[arr[x][y]+1] += 1;
+            return;
+        }
+
+        int m = n/3;
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                solve(arr, ans, x+i*m, y+j*m, m);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.valueOf(br.readLine());
+        int[][] arr = new int[n][n];
+        int[] ans = new int[3];
+
+        for(int i=0; i<n; i++){
+            String[] s = br.readLine().split(" ");
+            for(int j=0; j<n; j++){
+                arr[i][j] = Integer.valueOf(s[j]);
+            }
+        }
+
+        solve(arr, ans, 0, 0, n);
+
+        for(int i=0; i<3; i++){
+            System.out.println(ans[i]);
+        }
+
+    }
+}
